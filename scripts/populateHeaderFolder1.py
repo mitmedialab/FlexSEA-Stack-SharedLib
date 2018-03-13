dd1c7ff5b1840a5ccfea9dbeac88bcab8b05686e
Jean-François Duval <jfduval@dephy.com>
2018-03-13 16:53:45 -0400

    Working on the headers script. Status: folder structure replicated. Next: copy .h files.

--- scripts/populateHeaderFolder1.py
+++ scripts/populateHeaderFolder1.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+#Project status: code finds all relevant directories. No copying yet.
+
 #Find all directories in this path (recursive):
 def listDirectories(bp, dl):
 	a = os.listdir()
@@ -14,7 +16,6 @@ def listDirectories(bp, dl):
 	#print(dirList)
 	return dl
 
-
 def numOfDotH():
 	n = 0
 	for file in os.listdir():
@@ -25,22 +26,45 @@ def numOfDotH():
 #Takes a list of directories. If they have .h files keep them, otherwise remove them from the list
 def removeDirWithoutHeaders(d):
 	c = []
+	entryPath = os.getcwd()
 	#print('\n\n >>> removeDirWithoutHeaders:')
 	for u in range (0, len(d)):
+		#print("Calling chdir on:", d[u][1:])
 		os.chdir(d[u][1:])
 		currPath = os.getcwd()
 		#print("Searching in", currPath)
 		if numOfDotH():
 			c.append(d[u])
 		os.chdir('..')
+		os.chdir(entryPath) #Make sure to exit at the level we entered
 	return c
 	#print('<<<<<<<<<<<')
 
-print("\nPopulate Header Folder v0.1\n")
+#Creates new directories based on list
+def createNewDirectories(destPath, subm, d):
+	entryPath = os.getcwd()
+	os.chdir(destPath)
+	if not os.path.exists(subm):
+		os.makedirs(subm)
+	
+	os.chdir(subm)
+	currPath = os.getcwd()
+	print("Working in", currPath)
+	#At this point the submodule folder exists. Create sub-folders:
+	for u in range (0, len(d)):
+		if not os.path.exists(d[u][1:]):
+			os.makedirs(d[u][1:])
+	
+	os.chdir(entryPath) #Make sure to exit at the level we entered
+
+#Main:
+#===============================================
+
+print("\n>>> Populate Header Folder v0.1 <<<\n")
 basePath = os.getcwd()
 print(basePath)
 
-submodulesDir = ['flexsea-comm', 'flexsea-shared', 'flexsea-system', 'flexsea-user']
+submodulesDir = ['flexsea-comm', 'flexsea-shared', 'flexsea-system', 'flexsea-projects']
 
 #Prepare destination:
 directory = 'Headers'
@@ -64,8 +88,9 @@ for x in range(0,len(submodulesDir)):
 	print("Searching in", currPath)
 	dirList = []
 	d = listDirectories('/',dirList)
-	print('dirList:',d)
+	#print('dirList:',d)
 	cleanList = removeDirWithoutHeaders(d)
 	print('Clean list:',cleanList)
+	createNewDirectories(destPath, submodulesDir[x], cleanList)
 	os.chdir('..')
 	print('\n')
